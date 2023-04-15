3d708c95cea261c0afc10a953bef5f48ed88d089
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2023-04-15 16:09:26 +0100

    Update index.jl

--- index.jl
+++ index.jl
@@ -334,6 +334,7 @@ md"""
 
 - [Fastrack to `Julia`](https://juliadocs.github.io/Julia-Cheat-Sheet/) cheatsheet.
 - [MATLAB-`Julia`-Python](https://cheatsheets.quantecon.org/) by [QuantEcon group](https://quantecon.org) comparative cheatsheet
+- [Julia By Example](https://juliabyexample.helpmanual.io/)										
 - [MATLAB to `Julia` online converter](https://lakras.github.io/matlab-to-julia/)
 - [`Julia` Packages](https://juliapackages.com/packages?sort=stars)
 - [Basic Commands in `Pluto`](https://github.com/fonsp/Pluto.jl/wiki/%F0%9F%94%8E-Basic-Commands-in-Pluto)
